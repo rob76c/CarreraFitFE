4ac9951e3e95b0bd53576381f14a3cc905d39274
Robbie <rob76c@gmail.com>
2025-06-18 18:45:13 -0400

    List of exercises done

--- CarreraFitFrontEnd/App.js
+++ CarreraFitFrontEnd/App.js
@@ -1,18 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 import exercises from "./assets/data/exercises.json";
+import ExerciseListItem from "./src/components/ExerciseListItem";
 
 export default function App() {
-  const exercise = exercises[0];
-
   return (
     <View style={styles.container}>
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>{exercise.name}</Text>
-        <Text style={styles.exerciseSubtitle}>
-          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
-        </Text>
-      </View>
+      <FlatList
+        data={exercises}
+        contentContainerStyle= {{gap:5}}
+        //Use Key in the database: workoutID
+        keyExtractor={(item => item.name)}
+        renderItem={({ item }) => <ExerciseListItem item={item}/>}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -24,18 +25,6 @@ const styles = StyleSheet.create({
     backgroundColor: "gainsboro",
     justifyContent: "center",
     padding: 10,
-  },
-  exerciseContainer: {
-    backgroundColor: "#fff",
-    padding: 10,
-    borderRadius: 10,
-    gap: 5,
-  },
-  exerciseName: {
-    fontSize: 20,
-    fontWeight: "500",
-  },
-  exerciseSubtitle: {
-    color: "dimgray",
+    paddingTop: 70,
   },
 });
