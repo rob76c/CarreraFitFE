bbdc2bf8a11806377fbf61a709873726ef5b23ca
Robbie <rob76c@gmail.com>
2025-07-01 17:45:21 -0400

    setup expo router

--- CarreraFitFrontEnd/src/components/ExerciseListItem.jsx
+++ CarreraFitFrontEnd/src/components/ExerciseListItem.jsx
@@ -1,18 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
 
-
-
-export default function ExerciseListItem({item}) {
+export default function ExerciseListItem({ item }) {
   return (
     <View style={styles.exerciseContainer}>
       <Text style={styles.exerciseName}>{item.name}</Text>
       <Text style={styles.exerciseSubtitle}>
-        <Text style= {styles.subValue}>{item.muscle}</Text> | {' '} <Text style = {styles.subValue}>{item.equipment}</Text>
+        <Text style={styles.subValue}>{item.muscle}</Text> |{" "}
+        <Text style={styles.subValue}>{item.equipment}</Text>
       </Text>
     </View>
   );
 }
-
 
 const styles = StyleSheet.create({
   exerciseContainer: {
@@ -20,6 +18,18 @@ const styles = StyleSheet.create({
     padding: 10,
     borderRadius: 10,
     gap: 5,
+    marginHorizontal: 2,
+
+    //shadow
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
   },
   exerciseName: {
     fontSize: 20,
@@ -28,7 +38,7 @@ const styles = StyleSheet.create({
   exerciseSubtitle: {
     color: "dimgray",
   },
-  subValue :{
-    textTransform: 'capitalize',
+  subValue: {
+    textTransform: "capitalize",
   },
 });
