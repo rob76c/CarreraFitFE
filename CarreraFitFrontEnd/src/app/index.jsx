1e48c4cee2e56d6449c047eb9611e807b8dc8909
Robbie <rob76c@gmail.com>
2025-07-02 19:35:32 -0400

    details screen for exercise

--- CarreraFitFrontEnd/src/app/index.jsx
+++ CarreraFitFrontEnd/src/app/index.jsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import exercises from "../../assets/data/exercises.json";
-import ExerciseListItem from "../../src/components/ExerciseListItem";
+import ExerciseListItem from "../components/ExerciseListItem";
 
 export default function App() {
   return (
@@ -22,7 +22,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "ghostwhite",
+    // backgroundColor: "ghostwhite",
     justifyContent: "center",
     padding: 10,
   },
