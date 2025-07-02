61f6b668d533bb61be84d28944f1b4d59ec8edc9
Robbie <rob76c@gmail.com>
2025-07-02 19:44:09 -0400

    expanding instructions

--- CarreraFitFrontEnd/src/app/[name].jsx
+++ CarreraFitFrontEnd/src/app/[name].jsx
@@ -2,9 +2,12 @@ import { View, Text, StyleSheet, ScrollView } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import exercises from "../../assets/data/exercises.json";
 import { Stack } from "expo-router";
+import { useState } from "react";
 
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams();
+
+  const[isInstructionsExpanded, setIsInstructionsExpanded ] = useState(false);
 
   const exercise = exercises.find((item) => item.name == params.name);
 
@@ -26,7 +29,8 @@ export default function ExerciseDetailsScreen() {
       </View>
 
       <View style={styles.panel}>
-        <Text style={styles.instructions}>{exercise.instructions}</Text>
+        <Text style={styles.instructions} numberOfLines={isInstructionsExpanded ? 0 : 3}>{exercise.instructions}</Text>
+        <Text onPress = {() => setIsInstructionsExpanded(!isInstructionsExpanded)} style={styles.seeMore}>{isInstructionsExpanded ? 'See less' : 'See more'}</Text>
       </View>
     </ScrollView>
   );
@@ -56,5 +60,11 @@ const styles = StyleSheet.create({
   instructions: {
     fontSize: 16,
     lineHeight: 22,
+  },
+  seeMore: {
+    alignSelf: 'center',
+    padding: 5,
+    fontWeight: '600',
+    color:'gray'
   },
 });
